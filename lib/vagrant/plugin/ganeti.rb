c7362396dc7b88507fdffeeced673a6734241485
vagrant <vagrant@node1.example.org>
2013-06-21 18:02:44 +0000

    GemSpecs fixed

---
+++ lib/vagrant/plugin/ganeti.rb
@@ -0,0 +1,9 @@
+require "vagrant/plugin/ganeti/version"
+
+module Vagrant
+  module Plugin
+    module Ganeti
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/vagrant/plugin/ganeti/version.rb
@@ -0,0 +1,7 @@
+module Vagrant
+  module Plugin
+    module Ganeti
+      VERSION = "0.0.1"
+    end
+  end
+end
