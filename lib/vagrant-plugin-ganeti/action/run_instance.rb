838e9101d9d7c2ba7a22a72406335053fd072a2c
root <root@syst-Aspire-5738.(none)>
2013-07-03 14:25:59 +0530

    Working test1

---
+++ lib/vagrant-plugin-ganeti/action/run_instance.rb
@@ -0,0 +1,62 @@
+require "log4r"
+
+module VagrantPlugins
+  module GANETI
+    module Action
+      # "unlink" vagrant and the managed server
+      class RunInstance
+
+        def initialize(app, env)
+          @app    = app
+          @logger = Log4r::Logger.new("vagrant_ganeti::action::remove_instance")
+        end
+
+        def call(env)
+            config     = env[:machine].provider_config.get_config()
+	    client = env[:ganeti_compute]
+           # Launch!
+            env[:ui].info(I18n.t("vagrant_ganeti.launching_instance"))
+            env[:ui].info(" -- Username: #{config.username}")
+            env[:ui].info(" -- OS NAME: #{config.os_name}")
+            env[:ui].info(" -- Instance NAME: #{config.instance_name}")
+            env[:ui].info(" -- Host: #{config.host}")
+            env[:ui].info(" -- Primary Noode #{config.pnode}") 
+            env[:ui].info(" -- Disk Template: #{config.disk_template}")
+            env[:ui].info(" -- Disks #{config.disks}") 
+            env[:ui].info(" -- Network Configurations: #{config.nics}") 
+            env[:ui].info(" -- Version : #{config.version }") if config.version 
+
+            createjob = client.instance_create()
+	    env[:ui].info( "New Job Created #{createjob}.")
+ 	    env[:ui].info( "Creating Instance")
+	    env[:ui].info( "This might take few minutes.....")
+	    puts env[:ganeti_compute]
+	    while true
+		if  client.is_job_ready(createjob) == "error"
+			env[:ui].info("Error Creating instance")
+			break
+		elsif client.is_job_ready(createjob) == "running"
+			#Waiting for the message to succeed
+			sleep(15)
+		elsif client.is_job_ready(createjob) == "success" or  client.is_job_ready(createjob) == "already_exists"
+	    		env[:ui].info( "Instance sucessfully Created.")
+ 	    		env[:ui].info( "Booting up the Instance.")
+            		bootinstancejob = client.start_instance()
+			sleep(3)
+		      	if client.is_job_ready(bootinstancejob) == "success" 
+				env[:machine].id = client.info['nics'][0]['ip']
+			        env[:ui].info( "#{ env[:machine].id}")
+				env[:ui].info("Instance Started Sucessfully")
+		        else
+			 	env[:ui].info("Error Staring Instance")
+                        end 
+			break
+		end
+            end
+
+          @app.call(env)
+        end
+      end
+    end
+  end
+end
---
+++ lib/vagrant-plugin-ganeti/util/ganeti_client.rb
@@ -0,0 +1,134 @@
+module VagrantPlugins
+  module GANETI
+    module Util
+	class GanetiClient
+		attr_accessor :host, :username, :password, :version,:info
+		
+		def initialize(host, username, password,info)
+		    self.host = host
+		    self.username = username
+		    self.password = password
+		    self.info = info
+		    self.version = self.version_get
+		end
+
+	
+		def instance_create( dry_run = 0)
+		    url = get_url("instances")
+		    body = info.to_json
+		    response_body = send_request("POST", url, body)
+		    return response_body
+		end
+
+		def instance_terminate(dry_run = 0)
+		    url = get_url("instances/#{info['instance_name']}")
+		    body = info.to_json
+		    response_body = send_request("DELETE", url)
+		    return response_body
+		end
+
+
+		def is_job_ready(jobno)
+		    url = get_url("jobs/#{jobno}")
+		    response_body = send_request("GET", url)
+		    if response_body["status"] =="error"
+            		if response_body["opresult"][0][1][1] == "already_exists"
+				return "already_exists"
+			else
+				return "error"
+			end
+		    else
+			response_body["status"]
+		    end
+		end
+
+		def start_instance()
+		    url = get_url("instances/#{info['instance_name']}/startup")
+		    response_body = send_request("PUT", url)
+		    
+		    return response_body
+		end
+
+		def version_get
+		    url = get_url("version")
+		    response_body = send_request("GET", url)
+		    
+		    return response_body
+		end
+	
+		def authenticate(username, password)
+		    basic = Base64.encode64("#{username}:#{password}").strip
+		    return "Basic #{basic}"
+		end
+	    
+	
+		def get_url(path, params = nil)
+		    param_string = ""
+
+		    if params
+		        params.each do |key, value|
+		            if value.kind_of?(Array)
+		                value.each do |svalue|
+		                    param_string += "#{key}=#{svalue}&"
+		                end
+		            else
+		                param_string += "#{key}=#{value}&"
+		            end
+		        end
+		    end
+
+		     url =  (self.version)? "/#{self.version}/#{path}?#{param_string}" : "/#{path}?#{param_string}"
+	  
+		    return url.chop
+		end
+
+	
+		def send_request(method, url, body = nil, headers = {}) 
+		   uri = URI.parse(host)
+
+		   http = Net::HTTP.new(uri.host, uri.port)
+		   http.use_ssl = true
+		   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		   headers['User-Agent'] = 'Ruby Ganeti RAPI Client'
+		   headers['Content-Type'] = 'application/json'
+		   headers['Authorization']= authenticate(self.username, self.password)
+		  
+		   begin
+		       response = http.send_request(method, url, body, headers)
+		      # response = http.send_request("GET",url)
+		   rescue => e
+		        puts "Error sending request"
+			puts "#{e.message}"
+			       
+		    else
+		        case response
+		        when Net::HTTPSuccess
+		            parse_response(response.body.strip)
+		        else
+		            response.instance_eval { class << self; attr_accessor :body_parsed; end }
+		            begin 
+		                response.body_parsed = parse_response(response.body) 
+		            rescue
+		                # raises  exception corresponding to http error Net::XXX
+		               puts "#{response.error!}"
+		            end
+		        end
+		    end
+		end
+
+
+		def parse_response(response_body)
+		    # adding workaround becouse Google seems to operate on 'non-strict' JSON format
+		    # http://code.google.com/p/ganeti/issues/detail?id=117
+		    begin
+		        response_body = JSON.parse(response_body)
+		    rescue
+		        response_body = JSON.parse('['+response_body+']').first
+		    end
+
+		    return response_body
+		end
+	end #Class GanetiClient
+     end
+  end
+end
